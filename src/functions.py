dec7978c658b9bfed58f4a9c561add40fd76b9e3
David Bruce <david.bruce14@gmail.com>
2020-12-12 10:59:13 -0500

    first commit with webscraping function

---
+++ src/functions.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import twint
+import nest_asyncio
+nest_asyncio.apply()
+import datetime
+from dateutil.relativedelta import relativedelta
+
+# scraping function
+def twint_scrape(
+    search,
+    since=None,
+    until=None,
+    limit=None
+):
+    c = twint.Config()
+    c.Search = search
+    c.Lang = 'en'
+    c.Since = since
+    c.Until = until
+    c.Limit = limit
+    c.Pandas = True
+    c.Hide_output = True
+    
+    twint.run.Search(c)
+    
+    df = twint.storage.panda.Tweets_df
+    
+    return df
+
+
+# loop for scraping function
+def search_loop(
+    search,
+    filename,
+    limit=None
+):
+    
+    # instantiate empty dataframe
+    df = pd.DataFrame()
+    
+    for i in range(1,13):
+        
+        since = pd.datetime(2020,i,1).strftime('%Y-%m-%d')
+        
+        until = (pd.datetime(2020,i,1) + relativedelta(months=+1)).strftime('%Y-%m-%d')
+        
+        month_df = twint_scrape(
+            search = search,
+            since = since,
+            until = until,
+            limit = limit)
+        
+        df = pd.concat([df, month_df])
+        del month_df
+        df.reset_index(drop=True, inplace=True)
+        df.to_csv(f'./data/{filename}.csv')
+        
+        # notifier
+        print(f'{since} saved!')
+        
+    return df
